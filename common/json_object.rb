84a246f30c6ac9b867eebb7fd44fda64e8b4f3dc
zwyang <yangziwei@me.com>
2015-12-22 00:25:01 +0800

    add JSONObject

---
+++ common/json_object.rb
@@ -0,0 +1,39 @@
+# Javascript like object without any method declaraion.
+# Support obj.a,  obj.a=, obj.a? (only when a is boolean)
+class JSONObject < Hash
+	alias_method :__hash_respond_to?, :respond_to?
+
+	def respond_to?(symbol, include_all=false)
+		return true if super(symbol, include_all)
+		return false if symbol.nil?
+		return false if symbol[-1] == '!'
+		return true
+	end
+
+	def method_missing(symbol, *args)
+		return super(symbol, *args) if __hash_respond_to?(symbol, true)
+		# use super to raise error.
+		return super(symbol, *args) if symbol.nil?
+		return super(symbol, *args) if symbol[-1] == '!'
+		# handle hash operation.
+		if symbol[-1] == '?' && args.empty?
+			# self.xxx?
+			return super(symbol, *args) if symbol.size == 1
+			val = self.[] symbol[0..-2].to_sym
+			return true if val == true
+			return false if val.nil? || val == false
+			return super(symbol, *args)
+		elsif symbol[-1] == '=' && args.size == 1
+			# self.xxx = yyy
+			return super(symbol, *args) if symbol.size == 1
+			self.[]= symbol[0..-2].to_sym, args[0]
+			return args[0]
+		elsif args.empty?
+			# self.xxx
+			val = self.[] symbol
+			return val
+		else
+			return super(symbol, *args)
+		end
+	end
+end
