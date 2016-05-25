83ff0641803b080b9244d3887fcee4e73eeef76f
zwyang <yangziwei@me.com>
2016-05-25 15:46:40 +0800

    add clear_redis_by_path

--- common/util.rb
+++ common/util.rb
@@ -63,7 +63,12 @@ module CacheUtil
 
 	def clear_redis_by_table(table)
 		cmd = "local keys = redis.call('keys', ARGV[1]) for i=1,#keys,5000 do redis.call('del', unpack(keys, i, math.min(i+4999, #keys))) end return keys";
-		@redis.eval(cmd, [], ["SQL_BUFFER:#{table}:*"])
+		redis.eval(cmd, [], ["SQL_BUFFER:#{table}:*"])
+	end
+
+	def clear_redis_by_path(prefix)
+		cmd = "local keys = redis.call('keys', ARGV[1]) for i=1,#keys,5000 do redis.call('del', unpack(keys, i, math.min(i+4999, #keys))) end return keys";
+		redis.eval(cmd, [], ["#{prefix}:*"])
 	end
 end
 
