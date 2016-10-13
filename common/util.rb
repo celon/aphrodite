0e63af6d3f4eacc9ec8a3b4f763af19108995a1a
yangziwei@me.com <yangziwei@me.com>
2016-10-13 12:37:12 +0800

    FileUtil::tail added

--- common/util.rb
+++ common/util.rb
@@ -51,6 +51,36 @@ module LockUtil
 	end
 end
 
+module FileUtil
+	def tail(file, opt={})
+		verbose = opt[:verbose] == true
+		sleep_interval = opt[:interval] || 0.1
+	
+		f = nil
+		begin
+			f = File.open(file,"r")
+			# seek to the end of the most recent entry
+	# 		f.seek(0,IO::SEEK_END)
+		rescue Errno::ENOENT
+			sleep sleep_interval
+			retry
+		end
+	
+		ct = 0
+		loop do
+			select([f])
+			line = f.gets
+			if line.nil? || line.size == 0
+				sleep sleep_interval
+				next
+			end
+			yield line.strip if block_given?
+			puts "#{ct.to_s.ljust(5)}: #{line}" if verbose
+			ct += 1
+		end
+	end
+end
+
 module CacheUtil
 	def cache_client
 		redis
